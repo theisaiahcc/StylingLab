69ef11fbd91f6ac2a8c732206bcad2b5ffc98775
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-01-19 09:49:41 -0800

    Initial commit

---
+++ ModelBindingStyling-Lab/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingStyling_Lab.Models;
+using System.Diagnostics;
+
+namespace ModelBindingStyling_Lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserProfile()
+        {
+            UserProfile user = GetUserProfileData();
+            return View(user);
+        }
+
+        public IActionResult PrinterList()
+        {
+            IEnumerable<ThreeDPrinters> printers = GetPrinterList();
+            return View(printers);
+        }
+
+        /// <summary>
+        /// Returns a list of 3d printers with test data for use
+        /// on a view to practice modelbinding
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<ThreeDPrinters> GetPrinterList()
+        {
+            return new List<ThreeDPrinters>()
+            {
+                new ThreeDPrinters 
+                {
+                    BuildVolume = "180 x 180 x 180 mm",
+                    Price = 499.99,
+                    SKU = "SMLPRINTER7",
+                    Title = "The mini printer"
+                },
+                new ThreeDPrinters 
+                {
+                    BuildVolume = "300 x 300 x 300 mm",
+                    Price = 899.99,
+                    SKU = "MEDPRINTER12",
+                    Title = "Mega Printer"
+                },
+                new ThreeDPrinters 
+                {
+                    BuildVolume = "360 x 360 x 360 mm",
+                    Price = 999.99,
+                    SKU = "MEDPRINTER12P",
+                    Title = "Mega Printer Plus"
+                },
+                new ThreeDPrinters 
+                {
+                    BuildVolume = "360 x 360 x 500 mm",
+                    Price = 899.99,
+                    SKU = "GIGANTOR10",
+                    Title = "Mega Printer"
+                }
+            };
+        }
+
+        /// <summary>
+        /// This method returns hardcoded data for a UserProfile
+        /// to use with modelbinding on a view
+        /// </summary>
+        private UserProfile GetUserProfileData()
+        {
+            return new UserProfile()
+            {
+                DateOfBirth = new DateOnly(1815, 7, 1),
+                Email = "First.Programmer@gmail.com",
+                FullName = "Ada Lovelace",
+                GitHubUrl = "https://github.com/Octocat",
+                ImageUrl = "https://via.placeholder.com/150",
+                PhoneNumber = "(253) 555-1234",
+                UserProfileId = 10,
+                SkilledLanguages = new List<string> { "C#", "Java", "C++" }
+            };
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ModelBindingStyling-Lab/Models/ThreeDPrinters.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingStyling_Lab.Models
+{
+    /// <summary>
+    /// Represents an individual 3D printer
+    /// </summary>
+    public class ThreeDPrinters
+    {
+        /// <summary>
+        /// Stock Keeping Unit identifier
+        /// </summary>
+        public string SKU { get; set; } = null!;
+
+        /// <summary>
+        /// Title of the printer product
+        /// </summary>
+        public string Title { get; set; } = null!;
+
+        /// <summary>
+        /// Sale price
+        /// </summary>
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Build volume that can be printed
+        /// ex. 180 x 180 x 180 mm
+        /// </summary>
+        [Display(Name = "Build Volume")]
+        public string BuildVolume { get; set; } = null!;
+
+        /// <summary>
+        /// Product image of the printer
+        /// </summary>
+        [Display(Name = "Printer Image")]
+        public string PrinterImage { get; set; } = "https://via.placeholder.com/300";
+    }
+}
